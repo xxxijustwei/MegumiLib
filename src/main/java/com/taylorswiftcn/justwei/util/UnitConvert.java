9ee8526a7a10c9cb9103ecc16a153c5b3a331a5c
justwei <admin@taylorswiftcn.com>
2021-10-15 14:23:07 +0800

    [add] 单位换算

---
+++ src/main/java/com/taylorswiftcn/justwei/util/UnitConvert.java
@@ -0,0 +1,23 @@
+package com.taylorswiftcn.justwei.util;
+
+import java.text.DecimalFormat;
+
+public class UnitConvert {
+
+    private static final long million = 100000000;
+    private static final long tenThousand = 10000;
+    public static DecimalFormat decimalFormat = new java.text.DecimalFormat("0.00");
+
+    public static String getFormatLong(long input) {
+        boolean negative = input < 0;
+        input = Math.abs(input);
+
+        if (input > million) {
+            return (negative ? "-" : "") + decimalFormat.format((double) input / (double) million) + " M";
+        } else if (input > tenThousand) {
+            return (negative ? "-" : "") + decimalFormat.format((double) input / (double) tenThousand) + " W";
+        } else {
+            return (negative ? "-" : "") + input + "";
+        }
+    }
+}
